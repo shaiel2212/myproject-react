a1896cb534fec5eea271ad302a2c3b32b89a9f02
reuven <reubenachladive@gmail.com>
2022-08-22 20:21:12 +0300

    add JWT virdy to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,24 +6,34 @@ import { initDB } from "../db";
 import loginRouter from "./auth/index";
 import registerRouter from "./auth/index";
 import vacationV1 from "./routes/index";
+import { expressjwt, Request as JWTRequest } from "express-jwt";
+const verifyJwtMiddleware = expressjwt({
+  secret: process.env.SECRET,
+  algorithms: ["HS256"],
+});
 
 initDB();
 
 const port = process.env.PORT;
-console.log(port);
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/healthcheck", async (req, res) => {
-  return res.send("Api is working!");
-});
-
+app.use(
+  verifyJwtMiddleware.unless({
+    path: ["/register", "/login"],
+  })
+);
 app.use(loginRouter);
 app.use(registerRouter);
-app.use(vacationV1);
 
+app.use(vacationV1);
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).send("invalid token...");
+  }
+});
 app.listen(port || 5000, () => {
   console.log(`Server Running on Port ${port}`);
 });
